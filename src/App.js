26fe8439bbce2a1a5afd668ebb29bcd86d9e9fef
Vikram Reddy <vikramreddymokalla@gmail.com>
2025-04-29 13:58:48 +1000

    SCRUM-38 Updated routers for navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import RedLightGreenLight from './components/game/RedLightGreenLight';
+import RedLightGreenLight from './components/game/LightGameSinglePlayer';
+import MultiplayerLobby from './components/game/MultiplayerLobby';
+import MultiplayerRedLightGreenLight from './components/game/LightGameMultiPlayer';
+import LoadingOverlay from './components/LoadingOverlay';
 
 export default function App() {
   return (
@@ -10,7 +13,10 @@ export default function App() {
         <Route path='/' element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/TheSquidGameCDU" element={<HomePage />} />
-        <Route path="/game" element={<RedLightGreenLight />} />
+        <Route path="/SinglePlayerRedLightGreenLight" element={<RedLightGreenLight />} />
+        <Route path="/MultiplayerLobby" element={<MultiplayerLobby />} />
+        <Route path="/game/:roomCode" element={<MultiplayerRedLightGreenLight />} />
+        <Route path="/LoadingOverlay" element={<LoadingOverlay />} />
       </Routes>
     </Router>
   );
